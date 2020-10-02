d60c3224924992eca13793e7f2c018a801abe2ff
Hendy Irawan <hendy@hendyirawan.com>
2020-10-02 15:22:59 +0000

    docs: added tracing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
 using namespace std;
 typedef long long LL;
 typedef vector<int> vi;
@@ -37,15 +39,17 @@ struct dte{
 
 PII newEdge;
 
+/* Minimum cost maximum flow */
 struct MCMF {
     LL s, e, n;
     vector<vector<dte>> edge;
     MCMF(LL _s, LL _e, LL _n) {
         s = _s, e = _e, n = _n;
+        cout << "MCMF s(pasti 0)=" << _s << " e(jumlah city+1)=" << _e << " n(jumlah city+2)=" << _n << "\n";
         edge.resize(n);
     }
-    void addEdge(LL u, LL v, LL cap, LL cost) {
-        // cout << u << " " << v << " " << cap << " " << cost << endl;
+    void addEdge(/*source city*/ LL u, /*dest city*/ LL v, /*difference of degree*/ LL cap, LL cost) {
+        cout << "addEdge " << (u+1) << " to " << (v+1) << " : diff=" << cap << " cost=" << cost << endl;
         dte e1 = { v, cost, cap, 0,(LL)(edge[v].size())};
         dte e2 = { u, -cost, 0, 0,(LL)(edge[u].size())};
         edge[u].push_back(e1); edge[v].push_back(e2);
@@ -141,7 +145,7 @@ void hierholzer(int pos){
 }
 
 /** Floyd warshall */
-void generateDist(int n){
+void generateDist(/* number of cities */ int n){
     // rep(i,1,n+1) rep(j,1,n+1) cout << ((dis[i][j] == LINF) ? "∞" : to_string(dis[i][j])) << " \n"[j == n];
     rep(k,1,n+1) rep(i,1,n+1) rep(j,1,n+1) dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j]);
     // rep(i,1,n+1) rep(j,1,n+1) cout << ((dis[i][j] == LINF) ? "∞" : to_string(dis[i][j])) << " \n"[j == n];
@@ -149,7 +153,8 @@ void generateDist(int n){
 
 
 /** Solve Chinese postman problem */
-pair<LL, vi> solve(int n, int m, LL tot = 0){
+pair<LL, vi> solveProblem(int n, int m, LL tot = 0){
+    cout << "Solving n=" << (n) << " m=" << (m) << " tot=" << tot << "\n";
 
     // Reset all stuffs
     curans.clear();
@@ -157,28 +162,77 @@ pair<LL, vi> solve(int n, int m, LL tot = 0){
     rep(i,0,n+1) rep(j,0,n+1) dis[i][j] = LINF, dir[i][j] = 0;
     rep(i,0,n+1) dis[i][i] = 0;
 
+    /* for all roads */
     rep(i,0,m){
+
+        /*
+        curedge = {<source_city, dest_city> : weight}
+        */
         pair<PII,LL> curedge = edges[i];
+        /*
+        deg 0 : keluar
+        deg 1 : masuk
+        deg[0..1][idx_city] = degree of vertex
+        */
         deg[0][curedge.fi.fi]++, deg[1][curedge.fi.se]++;
+        /*
+        dis[idx source city][dest city] = jarak terpendek
+        */
         dis[curedge.fi.fi][curedge.fi.se] = min(dis[curedge.fi.fi][curedge.fi.se], curedge.se);
+        /*
+        kalau distance dari kota A ke B == bobot edge ini:
+        dir[source city][dest city] = idx jalan + 1 // rute langsung terpendek lewat jalan ini dari kota A ke B, ga muter2
+        */
         if(dis[curedge.fi.fi][curedge.fi.se] == curedge.se) dir[curedge.fi.fi][curedge.fi.se] = i+1;
+        /*
+        edge : vector[source city] = pair<dest city, idx jalan>
+        */
         edge[curedge.fi.fi].pb({curedge.fi.se, i+1});
+        //cout << edge[curedge.fi.fi] << "\n";
     }
 
+    /* memastikan dis(tance) terpendek dari setiap kombinasi kota I -> kota perantara K -> J.
+    masuk ke variable dis */
     generateDist(n);
 
     // This graph is not strongly connected
-    rep(i,1,n+1) rep(j,1,n+1) if(dis[i][j] == LINF) return {LINF, {}};
+    // for each combination of city I * J:
+    // checking kalau tidak mungkin dari kota I menuju J, maka program berhenti
+    rep(i,1,n+1) rep(j,1,n+1) if(dis[i][j] == LINF) {
+      cout << "No route from city " << (i+1) << " to " << (j+1) << " so will not recurse solve\n";
+      return {LINF, {}};
+    }
 
+    /*
+    _s = 0
+    _e = jumlah kota + 1
+    _n = jumlah kota + 2
+    */
     MCMF solve(0, n+1, n+2);
     int degdiff[2] = {0,0};
 
     for(int i = 1;i <= n;i++){
+        /* diff : selisih antara degree masuk(1) - degree keluar(0) */
         int diff = deg[1][i]-deg[0][i];
-        if(!diff) continue;
+        if(!diff) {
+          cout << "Degree masuk & keluar for city " << (i+1) << " is ZERO\n";
+          continue;
+        }
+        cout << "Degree masuk & keluar for city " << (i+1) << " is " << diff << "\n";
         degdiff[(diff > 0)] += abs(diff);
-        if(diff > 0) solve.addEdge(0, i, diff, 0), part[i] = -1;
-        else solve.addEdge(i, n+1, -diff, 0), part[i] = 1;
+        // selisih degree harus selalu positif untuk dari kota 1 -> I,
+        // kalo negatif maka jadi edge dari kota I+1 -> kota terakhir
+        /*
+        part[idx city] : awalnya 0, lalu bisa jadi -1 atau 1
+        */
+        if(diff > 0) {
+          solve.addEdge(0, i, diff, 0), part[i] = -1;
+          cout << "  part " << (i+1) << " = -1\n";
+        }
+        else {
+          solve.addEdge(i, n+1, -diff, 0), part[i] = 1;
+          cout << "  part " << (i+1) << " = 1\n";
+        }
     }
 
     rep(i,1,n+1) rep(j,1,n+1) if(part[i] == -1 && part[j] == 1) solve.addEdge(i, j, LINF, dis[i][j]);
@@ -210,11 +264,34 @@ void print(pair <LL, vi> &ans, bool swappedFirst = 0){
         if(!swappedFirst) cout << "BAIK" << endl;
         else cout << "BURUK" << endl;
         cout << ans.fi << endl;
-        cout<<ans.se.size()<<" "; rep(i,0,sz(ans.se)) cout << ans.se[i] << " \n"[i == sz(ans.se)-1];
+        cout<<ans.se.size()<<" ";
+        LL total_cost = 0;
+        rep(i,0,sz(ans.se)) {
+          pair<PII,LL> curedge = edges[ans.se[i]];
+          cout << ans.se[i] << "(" << curedge.se << ")" << " \n"[i == sz(ans.se)-1];
+          total_cost += curedge.se;
+        }
+        cout << "Total cost: " << total_cost << endl;
     }
 }
 
 int main(){
+
+    // read from case1.txt
+    // ifstream MyReadFile("case1.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    // while (getline (MyReadFile, myText)) {
+    //   // Output the text from the file
+    //   cout << myText;
+    // }
+
+    // Close the file
+    // string myText;
+    // MyReadFile.close();
+    // getline(MyReadFile, myText);
+    // cout << myText;
+
     int n, m;
     LL tot = 0;
     cin >> n >> m; N = n;
@@ -223,9 +300,17 @@ int main(){
         edges.pb({{u, v}, c}); tot += c;
         deg[0][u]++; deg[1][v]++;
     }
-    pair <LL, vi> a = solve(n,m,tot);
+    cout << "-------- NGECEK YANG BAIK ------\n";
+    pair <LL, vi> a = solveProblem(n,m,tot);
     swap(edges[0].fi.fi, edges[0].fi.se);
-    pair <LL, vi> b = solve(n,m,tot);
+    print(a);
+    cout << "Total BAIK:" << a.fi << endl;
+    cout << "-------- NGECEK YANG BURUK ------\n";
+    pair <LL, vi> b = solveProblem(n,m,tot);
+    print(b, 1);
+    cout << "Total BURUK:" << b.fi << endl;
+
+    cout << "BEST:\n";
     if(a.fi <= b.fi) print(a);
     else print(b, 1);
     return 0;
