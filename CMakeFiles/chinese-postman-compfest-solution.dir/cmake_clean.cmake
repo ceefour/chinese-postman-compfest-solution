bc80baf114ae34913569c6b2716fd0e7a5219098
Hendy Irawan <hendy@hendyirawan.com>
2020-10-02 14:18:19 +0000

    build: gdb

---
+++ CMakeFiles/chinese-postman-compfest-solution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chinese-postman-compfest-solution.dir/main.cpp.o"
+  "chinese-postman-compfest-solution"
+  "chinese-postman-compfest-solution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chinese-postman-compfest-solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
